d772d9565b00098ed4616b65505614c7f1f087ca
David Papamichael <davidpapamichael@gmail.com>
2020-04-22 12:05:38 +0100

    module now can have defined capacity

--- lib/bike_container.rb
+++ lib/bike_container.rb
@@ -3,7 +3,7 @@ module BikeContainer
 
   attr_reader :capacity
 
-  def initialize
-    @capacity = DEFAULT_CAPACITY
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
   end
 end
--- spec/support/shared_examples_for_bike_container.rb
+++ spec/support/shared_examples_for_bike_container.rb
@@ -1,5 +1,16 @@
 shared_examples_for BikeContainer do
-  it 'has a default capacity when initialized' do
-    expect(subject.capacity).to eq BikeContainer::DEFAULT_CAPACITY
+
+  describe 'capacity'do
+    it 'has a default capacity when initialized' do
+      expect(subject.capacity).to eq BikeContainer::DEFAULT_CAPACITY
+    end
+
+    it 'can be overidden on initialize' do
+      random_capacity = Random.rand(100)
+      subject = described_class.new(random_capacity)
+      expect(subject.capacity).to eq random_capacity
+    end
   end
+
+
 end
